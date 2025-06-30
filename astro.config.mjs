d66c2f8630a0dd5f029ecbe3ffc9e75d1fb9f1c8
neviglastvo <ParadiseSeeker94@Gmail.com>
2025-06-30 20:52:25 +0300

    init

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
+
+export default defineConfig({
+  integrations: [astroI18next()],
+  build: {
+    inlineStylesheets: 'never'
+  }
+});
